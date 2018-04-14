8d168df8e86655d5f72cbbf6dc62db652303e1cc
Javier Morato <javiermorato@gmail.com>
2018-04-14 07:04:40 -0400

    callback collection workouts and exercices, joint that!

--- src/app/components/series/series.component.ts
+++ src/app/components/series/series.component.ts
@@ -23,7 +23,7 @@ export class SeriesComponent implements OnInit {
 
   workoutCol: AngularFirestoreCollection<Workout>;
   workouts: Observable<Workout[]>;
-  test: any;
+  allWorkouts: any;
 
   constructor( private afs: AngularFirestore) { }
 
@@ -31,12 +31,58 @@ export class SeriesComponent implements OnInit {
     this.workoutCol = this.afs.collection('workouts');
     this.workouts = this.workoutCol.valueChanges();
 
-    this.afs.collection('workouts').ref.get().then(function(querySnapshot) {
+    this.allWorkouts = this.getAllData(function(allCompleteWorkouts) {
+      console.log(allCompleteWorkouts);
+      return allCompleteWorkouts;
+    });    
+  }
 
-      querySnapshot.forEach(function(doc) {
-          console.log(doc.id, " => ", doc.data());
+  getAllData(returnAll) {
+    let allWorkouts = [];
+    let allExercises = {};
+
+    this.callFireStoreArray('workouts', (response) => {
+      allWorkouts = response;
+
+      this.callFireStoreObjects('exercises', (response) => {
+        allExercises = response;
+
+        allWorkouts.forEach(function(workout, index) {
+          
+          workout.programme.training.forEach(function(training, index){
+            workout.programme.training[index].description = allExercises[training.item];
+          })
+        });
+
+        returnAll(allWorkouts);
       });
 
+    });
+  }
+
+
+  callFireStoreArray($path, success) {
+    let allData = [];
+    this.afs.collection($path).ref.get().then((query) => {
+      query.forEach(function(doc) {
+          let element = {};
+          element = doc.data();
+          element['id'] = doc.id;
+          allData.push(element);
+      });
+
+        success(allData);
+    });
+  }
+
+  callFireStoreObjects($path, success) {
+    let allData = {};
+    this.afs.collection($path).ref.get().then((query) => {
+      query.forEach(function(doc) {
+          allData[doc.id] = doc.data();
+      });
+
+        success(allData);
     });
   }
 }
