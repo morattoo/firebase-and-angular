0bcf26e16967d4b1c5fa06376573e00763bb1d95
Javier Morato <javiermorato@gmail.com>
2018-05-11 06:37:35 -0400

    working route detail workout

--- src/app/components/workout/workout.component.ts
+++ src/app/components/workout/workout.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-workout',
   templateUrl: './workout.component.html'
 })
 export class WorkoutComponent implements OnInit {
+  
+  public workoutId: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.workoutId = this.route.snapshot.paramMap.get('id');
+    console.log(this.workoutId);
   }
 
 }
