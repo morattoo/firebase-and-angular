cdfe63c9484b0ccf9c00b1722bcfa63ebf1d5147
Javier Morato <javiermorato@gmail.com>
2018-05-23 21:58:45 -0400

    get one workout firebase

--- src/app/components/workout/workout.component.ts
+++ src/app/components/workout/workout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
@@ -9,15 +11,28 @@ import 'rxjs/add/operator/switchMap';
 export class WorkoutComponent implements OnInit {
   
   public workoutId: string;
+  public workout: any;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private afs: AngularFirestore
   ) { }
 
   ngOnInit() {
     this.workoutId = this.route.snapshot.paramMap.get('id');
-    console.log(this.workoutId);
+    this.workout = this.afs.collection("workouts").doc(this.workoutId);
+
+    this.workout.ref.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
   }
 
 }
