7a1523fde3f3e6d82e530c13d1d96f47ded27b4c
Javier Morato <javiermorato@gmail.com>
2018-06-10 07:49:25 -0400

    working.. get training, remove video youtube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
--- src/app/components/workout/workout.component.ts
+++ src/app/components/workout/workout.component.ts
@@ -13,6 +13,7 @@ export class WorkoutComponent implements OnInit {
   
   public workoutId: string;
   public workout: any;
+  public dataTraining: any;
 
   private itemDoc: AngularFirestoreDocument<any>;
   item: Observable<any>;
@@ -28,10 +29,18 @@ export class WorkoutComponent implements OnInit {
   ngOnInit() {
     this.itemDoc = this.afs.doc<any>('workouts/'+ this.workoutId);
     this.item = this.itemDoc.valueChanges();
+    this.itemDoc.valueChanges().subscribe(item => this.getDataItemTraining(item));
   }
 
   log(event: number) {
      console.log(event);
   }
 
+  getDataItemTraining($data) {
+    this.dataTraining = $data.programme.training;
+  }
+
+  playWorkout() {
+    console.log(this.dataTraining);
+  }
 }
